03d8cd9c81812ebaed2ad3bbf2231589c5bf518d
Seve <sdkim@thoughtworks.com>
2016-12-13 12:46:41 -0500

    Seve & True | Added google search test in smoke suite

--- test/smoke.js
+++ test/smoke.js
@@ -1,10 +1,21 @@
 module.exports = {
   'Open Google in chrome' : function (browser) {
-      browser
-          .windowMaximize()
-          .url(browser.launch_url)
-          .waitForElementVisible('body', 5000, 'Browser opened')
-          .waitForElementVisible('div[id=main]', 5000, 'Page components rendered')
-          .end();
+    browser
+      .windowMaximize()
+      .url(browser.launch_url)
+      .waitForElementVisible('body', 5000, 'Browser opens')
+      .waitForElementVisible('div[id=main]', 5000, 'Page components renders')
+      .end();
+  },
+
+  'Google Search "Pineapple"' : function(browser) {
+    browser
+      .windowMaximize()
+      .url(browser.launch_url)
+      .setValue('#lst-ib','Pineapple')
+      .sendKeys('#lst-ib', browser.Keys.ENTER)
+      .waitForElementVisible('#rhscol', 5000, 'Search page renders')
+      .assert.containsText('#rhs_block > div.g.rhsvw.kno-kp.mnr-c.g-blk > div.kp-blk._Jw._Rqb._RJe > div > div._OKe > div:nth-child(2) > div.kp-header > div > div.kp-hc > div > div > div._Q1n > div.kno-ecr-pt.kno-fb-ctx', 'Pineapple', 'Google search for "Pineapple" completes')
+      .end();
   }
 };
